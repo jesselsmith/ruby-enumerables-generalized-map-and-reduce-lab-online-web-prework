549fefe81f467abf1af796cba38e8cea49f2da90
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-27 00:31:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+def map(array_in)
+  array_out = []
+  array_in.size.times do |counter|
+    array_out << yield(array_in[counter])
+  end
+  array_out
+end
+
+def reduce(array_in, start = 0)
+  if(start = )
+  output = start
+  array_in.size.times do |counter|
+    output = yield(output, array_in[counter])
+  end
+  output
+end
